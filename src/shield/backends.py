9be5d2f7bdda639df2989db358e6b26ca3c0a83d
Ryan Leckey <rleckey@concordusapps.com>
2013-03-01 23:04:12 -0800

    Updated; implemented has_perms.

--- src/shield/backends.py
+++ src/shield/backends.py
@@ -34,7 +34,31 @@ class Backend(object):
         qset = obj.__class__.objects.filter(rule(user)).filter(pk=obj.pk)
         return qset.exists()
 
-    def manager_for_perm(self, manager, perm, user):
+    def has_perms(self, user, perm_list, obj=None):
+        if obj is None:
+            # No object; django - you do it
+            return False
+
+        if user and not user.is_anonymous() and not user.is_active:
+            # inactive and anonymous users never have permissions
+            return False
+
+        # Iterate through permissions
+        objects = obj.__class__.objects
+        for perm in perm_list:
+            # retrieve the rule
+            rule = registry.get((perm, obj.__class__))
+            if rule is None:
+                # No rule; no permission
+                return False
+
+            # execute and check for existance
+            objects = objects.filter(rule(user)).filter(pk=obj.pk)
+
+        # Check for existance
+        return objects.exists()
+
+    def objects_for_perm(self, manager, perm, user):
         if user and not user.is_anonymous() and not user.is_active:
             # inactive and anonymous users never have permissions
             return manager.none()
@@ -48,13 +72,13 @@ class Backend(object):
         # execute and retrieve users
         return manager.filter(rule(user))
 
-    def manager_for_perms(self, manager, perms, user):
+    def objects_for_perms(self, manager, perms, user):
         if user and not user.is_anonymous() and not user.is_active:
             # inactive and anonymous users never have permissions
             return manager.none()
 
         # retrieve the rules and build the qset
-        expression = None
+        objects = manager
         for perm in perms:
             rule = registry.get((perm, manager.model))
             if rule is None:
@@ -62,8 +86,7 @@ class Backend(object):
                 return manager.none()
 
             # Apply rule
-            q = rule(user)
-            expression = q if not expression else expression & q
+            objects = objects.filter(rule(user))
 
         # execute and retrieve users
-        return manager.filter(expression)
+        return objects
--- src/shield/models.py
+++ src/shield/models.py
@@ -5,22 +5,24 @@ from django.contrib import auth
 
 def for_perm(self, perm, user):
     for backend in auth.get_backends():
-        if hasattr(backend, "manager_for_perm"):
-            return backend.manager_for_perm(self, perm, user)
+        if hasattr(backend, "objects_for_perm"):
+            return backend.objects_for_perm(self, perm, user)
 
     return self.none()
 
 
 def for_perms(self, perms, user):
     for backend in auth.get_backends():
-        if hasattr(backend, "manager_for_perm"):
-            return backend.manager_for_perms(self, perms, user)
+        if hasattr(backend, "objects_for_perm"):
+            return backend.objects_for_perms(self, perms, user)
 
     return self.none()
 
 
-# Massive hacks beware
-setattr(manager.Manager, 'for_perm', for_perm)
-setattr(manager.Manager, 'for_perms', for_perms)
-setattr(QuerySet, 'for_perm', for_perm)
-setattr(QuerySet, 'for_perms', for_perms)
+# Massive hacks beware...
+# Waiting on a PR to django to remove this file
+if not hasattr(manager.Manager, 'for_perm'):
+    setattr(manager.Manager, 'for_perm', for_perm)
+    setattr(manager.Manager, 'for_perms', for_perms)
+    setattr(QuerySet, 'for_perm', for_perm)
+    setattr(QuerySet, 'for_perms', for_perms)
