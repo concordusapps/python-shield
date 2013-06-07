ad7c8a06b6543b88fbace6a46d13e5421271b8a2
Erich Healy <ErichRHealy@gmail.com>
2013-06-07 16:22:49 -0700

    Update the setup file to relate to the new project

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 #! /usr/bin/env python
 import os
-import re
 from setuptools import setup, find_packages
 
 
 setup(
-    name='django-shield',
+    name='python-shield',
     version='0.6.0',
-    description='An object-level permissions backend for django using expressive rules.',
+    description='An object-level permissions backend for django/sqlalchemy using expressive rules.',
     license='MIT',
     author='Concordus Applications',
     author_email='support@concordusapps.com',
-    url='http://github.com/concordusapps/django-shield/',
+    url='https://github.com/CactusCommander/python-shield',
     package_dir={'shield': 'src/shield'},
     packages=find_packages('src'),
     install_requires=[
         'django >= 1.5',
+        'six >= 1.3'
     ],
 )
