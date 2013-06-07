626b12e3a7b53d761f51bf169427f396c7496c67
Erich Healy <ErichRHealy@gmail.com>
2013-06-07 16:23:26 -0700

    Improve the registry to handle both single and dual permissions

--- src/shield/__init__.py
+++ src/shield/__init__.py
@@ -3,33 +3,74 @@ from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
 import functools
 import inspect
-from .rules import registry
+from . import rules
 
 
-class _wrapper:
+# class _wrapper:
 
-    def __init__(self, permissions, function):
-        self.function = function
+#     def __init__(self, permissions, function):
+#         self.function = function
+#         self.permissions = permissions
+
+#     def __call__(self, *args, **kwargs):
+#         return self.function(*args, **kwargs)
+
+
+# class rule:
+
+#     def __init__(self, *permissions):
+#         self.permissions = permissions
+
+#     def __call__(self, function):
+#         return _wrapper(self.permissions, function)
+
+
+# def rules(cls):
+#     for name, member in inspect.getmembers(cls):
+#         if isinstance(member, _wrapper):
+#             method = functools.partial(member.function, cls)
+#             for permission in member.permissions:
+#                 registry[(permission, cls)] = method
+
+#     return cls
+
+
+class _method_wrapper(object):
+
+    def __init__(self, permissions, fn):
         self.permissions = permissions
+        self.fn = fn
 
     def __call__(self, *args, **kwargs):
-        return self.function(*args, **kwargs)
+        return self.fn(*args, **kwargs)
 
 
 class rule:
 
-    def __init__(self, *permissions):
+    def __init__(self, owner, permissions, target=None):
+        """owner: The owner of the permissions.
+        permissions: The set of permissions that the owner has
+        target: The model that we are checking if the owner has permissions for
+        """
+        self.owner = owner
         self.permissions = permissions
+        self.target = target
 
-    def __call__(self, function):
-        return _wrapper(self.permissions, function)
+    def __call__(self, fn):
+        return _method_wrapper(self.permissions, fn)
 
 
 def rules(cls):
-    for name, member in inspect.getmembers(cls):
-        if isinstance(member, _wrapper):
-            method = functools.partial(member.function, cls)
-            for permission in member.permissions:
-                registry[(permission, cls)] = method
 
-    return cls
+    mems = inspect.getmembers(cls, lamdbda x: isinstance(x, _method_wrapper))
+    for name, member in mems:
+
+        # We can't add bound methods to our registry, so create partials
+        # that emulate bound methods
+        method = functools.partial(member.function, cls)
+
+        # Depending on the target, add to the single or dual
+        for permission in member.permissions:
+
+            # Add the permission to the registry
+            owner = registry.get(cls)
--- src/shield/rules.py
+++ src/shield/rules.py
@@ -2,5 +2,46 @@
 from __future__ import print_function, unicode_literals
 from __future__ import absolute_import, division
 
+# Two way permission registry.  For testing if an object has a permission on
+# a different object.
+# This is set up in the following format.
+# {
+#     owner: {
+#         target: {
+#             permission: function,
+#         }
+#     }
+# }
+dual = {}
 
-registry = {}
+# One way permission registry.  For checking if an object is part of a
+# permission group.
+# This is set up in the following format.
+# {
+#     owner: {
+#         permission: function,
+#     }
+# }
+single = {}
+
+
+def dual_add(owner, target, permission, function):
+    """Add a permission method to the dual hash"""
+
+    # Unwrap all the dictionaries
+    owner_dict = dual.get(owner, {})
+    target_dict = owner_dict.get(target, {})
+
+    # Set the permission
+    target_dict[permission] = function
+
+    # Wrap them back up
+    owner_dict[target] = target_dict
+    dual[owner] = owner_dict
+
+
+def single_add(owner, permission, function):
+    """Add a permission method to the single hash"""
+    owner_dict = single.get(owner, {})
+    owner_dict[permission] = function
+    single[owner] = owner_dict
