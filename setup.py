3e4a6610cabdffb1104f8adce4eada8bfc3f85d8
Ryan Leckey <rleckey@concordusapps.com>
2013-07-20 15:08:32 -0700

    Make python 2.x compatible.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ BASE_DIR = path.abspath(path.dirname(__file__))
 
 # Navigate, import, and retrieve the version of the project.
 VERSION = get_importer(path.join(BASE_DIR, 'src', 'shield')).find_module(
-    'meta').load_module().version
+    'meta').load_module('meta').version
 
 setup(
     name='shield',
